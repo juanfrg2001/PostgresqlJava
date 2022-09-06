26b87a49151329803da8b1ed1e3eeab1af6e6e9a
JuanAraque123 <65414006+JuanAraque123@users.noreply.github.com>
2022-09-06 16:44:44 -0500

    Update view.java

--- app/view/view.java
+++ app/view/view.java
@@ -1,9 +1,51 @@
 package app.view;
 
-import java.util.Scanner;
+package co.edu.unbosque.view;
 
-public class view {
+import javax.swing.JFrame;
 
-
-    
+public class View extends JFrame{
+	
+	private PanelInicio pInicial;
+	private PanelPrincipal pPrincipal;
+	/**
+	 * Constructor de la clase View 
+	 */
+	public View()
+	{	
+		
+		setSize(800,550);
+		setTitle("Base de Datos para Nomina");
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setLayout(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		inicializarComponentes();
+		setVisible(true);
+		
+	}
+	/**
+	 * Este metodo se encarga de inicializar componentes y es implementando de la interface Size
+	 * 
+	 */
+	
+	public void inicializarComponentes() {
+		pInicial = new PanelInicio();
+		pInicial.setVisible(false);
+		
+		pPrincipal = new PanelPrincipal();
+		pPrincipal.setVisible(true);
+		
+		
+		getContentPane().add(pInicial);
+		getContentPane().add(pPrincipal);
+	}
+		
+	public PanelPrincipal getpPrincipal() {
+		return pPrincipal;
+	}
+	
+	public PanelInicio getpRegistro() {
+		return pInicial;
+	}
 }
